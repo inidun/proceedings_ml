180bfc499e4706c22cd110a6b880dfc2ba636ab4
aibakeneko <aibakeneko@gmail.com>
2023-12-05 16:35:09 +0100

    Added encoding

--- proceedings_ml/scripts/text_folder_to_jsonl.py
+++ proceedings_ml/scripts/text_folder_to_jsonl.py
@@ -19,7 +19,7 @@ def create_jsonl_from_folder(folder_path: str, output_file: str) -> None:
         for file_name in os.listdir(folder_path):
             if file_name.endswith('.txt'):
                 file_path = os.path.join(folder_path, file_name)
-                with open(file_path, 'r') as file:
+                with open(file_path, 'r', encoding='utf-8') as file:
                     content = file.read()
                     writer.write({'text': content, 'meta': {'file_name': file_name}})
 
